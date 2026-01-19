5b18a0b37fe4f829b4ba20438eecd21e159f4412
yy3 <yuyue0534@gmail.com>
2026-01-19 18:25:53 +0800

    Add files via upload

---
+++ form-designer/vite.config.js
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  
+  // Tauri expects a fixed port
+  server: {
+    port: 5173,
+    strictPort: true,
+  },
+  
+  // Environment variables prefix
+  envPrefix: ['VITE_', 'TAURI_'],
+  
+  build: {
+    // Tauri uses Chromium on Windows and WebKit on macOS and Linux
+    target: process.env.TAURI_PLATFORM == 'windows' ? 'chrome105' : 'safari13',
+    // Don't minify for debug builds
+    minify: !process.env.TAURI_DEBUG ? 'esbuild' : false,
+    // Produce sourcemaps for debug builds
+    sourcemap: !!process.env.TAURI_DEBUG,
+  },
+});
